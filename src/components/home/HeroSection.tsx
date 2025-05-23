9a5776d01f45cf9fd439f1d9e33f22887bd78873
Asher Pope <asher@asherpope.com>
2025-05-23 02:46:23 -0400

    fix link lint error

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link';
+
 const HeroSection = () => {
   return (
     <section className="flex flex-col items-center text-center gap-6 w-full px-4 py-8">
       <h1 className="text-blue font-rubik text-2xl font-medium uppercase leading-8">
         LOREM IPSUM DOLOR SIT AMET CONSECTETUR
       </h1>
-      <a href="/blog" className="bg-main-accent text-white font-sans font-medium uppercase px-4 py-2 rounded-md">
+      <Link href="/blog" className="bg-main-accent text-white font-sans font-medium uppercase px-4 py-2 rounded-md">
         View Blog
-      </a>
+      </Link>
     </section>
   );
 };
